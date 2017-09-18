6796b8940e173962a471cd0bdff965c25f678918
Olayinka Kunle <olayinkakunle24@gmail.com>
2017-09-18 12:01:28 +0100

    Initial template commit

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <base href="<?=$this->config->item('base_url')?>" />
     <title>Abz | Game Portal</title>
 
     <meta name="description" content="Abz - Online game portal">
